cf09642cabddad6696e2174794edff8634aeda78
awoken-coder <jfafandrade@gmail.com>
2021-01-28 13:13:51 +0000

    new recipe page fundamental elements done

--- frontend/src/components/NewRecipePage.js
+++ frontend/src/components/NewRecipePage.js
@@ -12,27 +12,55 @@ const NewRecipePage = ({ editPageData }) => {
     origin: '',
     time: '',
   })
-  const [newIngredient, setNewIngredient] = useState({})
+  const [newIngredient, setNewIngredient] = useState({
+    ingredient: '',
+    quantity: 0,
+    unit: ''
+  })
   const [newCategory, setNewCategory] = useState('')
 
-  
+
   const handleSubmit = event => {
     event.preventDefault()
     return ''
   }
+  const handleInputIngredientChange = event => {
+    const target = event.target
+    let copyObject = JSON.parse(JSON.stringify(newIngredient))
+    switch (target.name) {
+      case 'unit':
+      case 'ingredient':
+        copyObject[target.name] = target.value
+        setNewIngredient(copyObject)
+        break;
+      case 'quantity':
+        const parsedQuantity = parseFloat(target.value)
+        if (isNaN(parsedQuantity)) {
+          console.log('deu nan', copyObject)
+          return setNewIngredient(copyObject)
+        }
+        copyObject.quantity = parsedQuantity
 
+        setNewIngredient(copyObject)
+        break;
+      default:
+        console.log('handleInputIngredient went to default')
+    }
+  }
   const handleInputChange = event => {
     const target = event.target
     let copyObject = JSON.parse(JSON.stringify(newRecipe))
+    console.log('copyObject', copyObject)
     console.log('handle input change target', target)
     switch (target.name) {
       case 'servings':
         console.log('case servings')
-        if (target.value === '') {
-          copyObject.ingredientsInfo.servings = target.value
-          setNewRecipe(copyObject)
+        const newServing = parseFloat(target.value)
+        console.log(newServing, 'newServing', isNaN(newServing))
+        if (isNaN(newServing)) {
+          console.log('deu nan', copyObject)
+          return setNewRecipe(copyObject)
         }
-        const newServing = parseInt(target.value)
         copyObject.ingredientsInfo.servings = newServing
         setNewRecipe(copyObject)
         break;
@@ -55,33 +83,66 @@ const NewRecipePage = ({ editPageData }) => {
         break;
     }
   }
-  const addIngredient = event => {
 
+  const createCategoryItems = (categories) => {
+    return categories.map((category, index )=> {
+      console.log('category', category)
+      return <li key={index}>{category}</li>
+    })
   }
   const addNewCategory = () => {
     const arrNewCategory = newRecipe.categories.concat([newCategory])
-    
-    let copyObject = JSON.parse(JSON.stringify(  newRecipe))
+
+    let copyObject = JSON.parse(JSON.stringify(newRecipe))
     copyObject['categories'] = arrNewCategory
     setNewRecipe(copyObject)
     setNewCategory('')
   }
-
+  const createIngredientItems = (ingredients) => {
+    return ingredients.map(({ingredient, quantity, unit},index) => {
+      console.log('ingredient', ingredient)
+      return <li key={index}>{ingredient}, {quantity} {unit}</li>
+    })
+  }
+  const addNewIngredient = () => {
+    if (newIngredient.ingredient === '' || newIngredient.unit === '' || newIngredient.quantity === 0) {
+      return ''
+    }
+    let copyObject = JSON.parse(JSON.stringify(newRecipe))
+    copyObject.ingredientsInfo.ingredients = copyObject.ingredientsInfo.ingredients.concat([newIngredient])
+    console.log('copyObject', copyObject.ingredientsInfo['ingredients'])
+    console.log('copyObject', copyObject)
+    setNewRecipe(copyObject)
+    setNewIngredient({
+      ingredient: '',
+      quantity: 0,
+      unit: ''
+    })
+  }
   return (
     <div>
-      New recipe Page
+      <h2>New recipe Page</h2>
       <form onSubmit={handleSubmit}>
         <ul>
           <li> Name: <input type='text' name='name' value={newRecipe.name} onChange={handleInputChange} /> </li>
           <li> Servings: <input type='number' name='servings' value={newRecipe.ingredientsInfo.servings} onChange={handleInputChange} /> </li>
-          {/*     <li>Ingredients: <input type='text' onChange={handleInputChange} />
-            <ul className='ingredients-list'></ul>
-          </li> */}
+          <ul className='ingredients-list'>
+            <h3>Ingredients</h3>
+            <ul>
+              {createIngredientItems(newRecipe.ingredientsInfo.ingredients)}
+            </ul>
+            <li>ingredient: <input type='text' name='ingredient' value={newIngredient.ingredient} onChange={handleInputIngredientChange} /> </li>
+            <li>quantity: <input type='number' name='quantity' value={newIngredient.quantity} onChange={handleInputIngredientChange} /> </li>
+            <li>unit: <input type='text' name='unit' value={newIngredient.unit} onChange={handleInputIngredientChange} /> </li>
+            <li><button onClick={addNewIngredient}>Add ingredient</button></li>
+          </ul>
           <li>Steps: <input type='text' name='steps' value={newRecipe.steps} onChange={handleInputChange} /></li>
           <li>Categories:
             <input type='text' name='categories' value={newCategory} onChange={handleInputChange} />
             <button onClick={addNewCategory}> Add category</button>
-
+            <ul>
+              {createCategoryItems(newRecipe.categories)}
+            </ul>
           </li>
           <li>Origin: <input type='text' name='origin' value={newRecipe.origin} onChange={handleInputChange} /></li>
           <li>Time: <input type='text' name='time' value={newRecipe.time} onChange={handleInputChange} /></li>
